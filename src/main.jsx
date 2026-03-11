459f7392e0c563a3d964e50dbce967099c157c57
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-03-11 09:12:56 +0530

    wrapped app with tourism context provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
+import TourismContextProvider from "./context/TourismContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <TourismContextProvider>
+        <App />
+      </TourismContextProvider>
     </BrowserRouter>
   </StrictMode>,
 );
