00d7714df8fb84c0ab8b7549589c7b79c39abe3d
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-03-11 09:54:24 +0530

    removed legent

--- src/components/LoginModal.jsx
+++ src/components/LoginModal.jsx
@@ -51,8 +51,7 @@ const LoginModal = () => {
 
           <form onSubmit={handleSubmit} className="">
             <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-              <legend className="fieldset-legend">Login</legend>
-
+              
               <label className="label">Email</label>
               <input
                 type="email"
