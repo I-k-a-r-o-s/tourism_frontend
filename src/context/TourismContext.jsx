f7df3a5d9832e7d117fdb590466d4416a08da093
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-03-11 09:48:02 +0530

    added axios and required variables ad functions

--- src/context/TourismContext.jsx
+++ src/context/TourismContext.jsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
 import { createContext } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router";
 
 export const TourismContext = createContext();
 
 const TourismContextProvider = ({ children }) => {
-  const tourismValue = {};
+  const [loading, setLoading] = useState(false);
+
+  const tourismApi = axios.create({
+    baseURL: import.meta.env.VITE_BASE_URL,
+    withCredentials: true,
+  });
+
+  const navigate = useNavigate();
+
+  const tourismValue = {
+    loading,
+    setLoading,
+    tourismApi,
+    navigate,
+  };
   return (
     <TourismContext.Provider value={tourismValue}>
       {children}
