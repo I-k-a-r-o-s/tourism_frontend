18678ba2990dc92ed7d710edf13452316495217f
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-03-11 08:53:25 +0530

    added basic routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
+import Navbar from "./components/Navbar";
+import { Toaster } from "react-hot-toast";
+import { Route, Routes } from "react-router";
+import Home from "./pages/Home";
+import ListingDetails from "./pages/ListingDetails";
+
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
-export default App
+    <div>
+      <Toaster />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/getlisting/:id" element={<ListingDetails />} />
+      </Routes>
+    </div>
+  );
+};
+export default App;
