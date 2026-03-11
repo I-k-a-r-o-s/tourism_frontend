7298d587c3c4064094feedb880f75621d6a68e4b
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-03-11 09:13:18 +0530

    basic login modal

--- src/components/LoginModal.jsx
+++ src/components/LoginModal.jsx
@@ -1,6 +1,60 @@
+import { useState } from "react";
+import { toast } from "react-hot-toast";
+
 const LoginModal = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!formData.email || !formData.password) {
+      toast.error("Please fill in all fields");
+      return;
+    }
+  };
+
   return (
-    <div>LoginModal</div>
-  )
-}
-export default LoginModal
+    <div>
+      {/* Open the modal using document.getElementById('ID').showModal() method */}
+      <button
+        className="btn"
+        onClick={() => document.getElementById("login_modal").showModal()}
+      >
+        Login
+      </button>
+      <dialog id="login_modal" className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Hello!</h3>
+
+          <form onSubmit={handleSubmit} className="">
+            <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+              <legend className="fieldset-legend">Login</legend>
+
+              <label className="label">Email</label>
+              <input type="email" className="input" placeholder="Email" />
+
+              <label className="label">Password</label>
+              <input type="password" className="input" placeholder="Password" />
+
+              <button type="submit" className="btn btn-neutral mt-4">
+                Login
+              </button>
+            </fieldset>
+          </form>
+
+          <div className="modal-action">
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                ✕
+              </button>
+            </form>
+          </div>
+        </div>
+      </dialog>
+    </div>
+  );
+};
+export default LoginModal;
